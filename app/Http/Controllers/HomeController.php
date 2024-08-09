51e225fcd14e4fa3ea52bfad4666bd71d33fe8aa
Slobozhancky <il.slob93@gmail.com>
2024-08-09 15:38:25 +0300

    Пройшов 'Директиви в Laravel'

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Http;
+
 class HomeController extends Controller
 {
     public function index()
     {
+//        $users = json_decode(file_get_contents("https://jsonplaceholder.typicode.com/users"));
+        $users = Http::get("https://jsonplaceholder.typicode.com/users")->json();
         $title = 'Home page';
-        return view('home.index', ['title' => $title]);
+        return view('home.index', compact('users', 'title'));
     }
 
     public function contacts()
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,8 +1,25 @@
 @extends('layouts.main')
 
+@section('title', $title)
+
 @section('home')
     <h1>Home page</h1>
+
+    <div class="container">
+        <div class="row">
+            @foreach($users as $user)
+                <div class="col-md-4 mb-2">
+                    <div class="card" style="width: 18rem;">
+                        <div class="card-body">
+                            <h5 @class(['text-success' => $loop->even, 'text-danger' => $loop->odd]) class="card-title">{{ $user['name'] }}; User ID: {{ $user['id'] }}</h5>
+                            <a href="tel:{{ $user['phone'] }}">{{ $user['phone'] }}</a>
+                            <a href="mailto:{{ $user['email'] }}">{{ $user['email'] }}</a>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
 @endsection
 
 
-@section('title', $title)
